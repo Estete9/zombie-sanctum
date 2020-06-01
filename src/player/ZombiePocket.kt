8b1eeb1a3ed64a697d001c4e348aa0da2e6f5991
Esteban <cepalacios@outlook.com>
2020-05-31 23:00:49 -0500

    Revert "successfully changed the zombie pocket and zombie lists into an array list of pairs(ZombiePocket, Zombie). Pending to update the zombie bite>when to use the new array list"
    This reverts commit 5ec7d5b2ed0d6f2c23c102a81acd4d0c5dab9513.

--- src/player/ZombiePocket.kt
+++ src/player/ZombiePocket.kt
@@ -2,9 +2,9 @@ package player
 
 import zombies.Zombie
 
-var zombieArrayListPairs = arrayListOf<Pair<ZombiePocket, Zombie>>()
-//val zombiePocketInventory = mutableListOf<ZombiePocket>()
-//val zombieInventory = mutableListOf<Zombie>()
+
+val zombiePocketInventory = mutableListOf<ZombiePocket>()
+val zombieInventory = mutableListOf<Zombie>()
 
 data class ZombiePocket(var zombie: Zombie) {
     val petZombieName = zombie.name
@@ -14,29 +14,19 @@ data class ZombiePocket(var zombie: Zombie) {
 }
 
 fun addZombie(zombie: Zombie) {
-    zombieArrayListPairs.add(Pair(ZombiePocket(zombie), zombie))
-//    zombiePocketInventory.add(ZombiePocket(zombie))
-//    zombieInventory.add(zombie)
+    zombiePocketInventory.add(ZombiePocket(zombie))
+    zombieInventory.add(zombie)
 }
 
 
 fun retrieveZombie(zombieName: String): Zombie {
-
-
     var selectedZombieIndex = 0
+    for (element in zombiePocketInventory) {
+        if (element.petZombieName == zombieName) {
+            selectedZombieIndex = zombiePocketInventory.indexOf(element)
 
-    for (element in zombieArrayListPairs){
-        if(element.first.petZombieName == zombieName){
-            selectedZombieIndex = zombieArrayListPairs.indexOf(element)
         }
     }
-return zombieArrayListPairs[selectedZombieIndex].second
-//    for (element in zombiePocketInventory) {
-//        if (element.petZombieName == zombieName) {
-//            selectedZombieIndex = zombiePocketInventory.indexOf(element)
-//
-//        }
-//    }
-//
-//    return zombieInventory[selectedZombieIndex]
+
+    return zombieInventory[selectedZombieIndex]
 }
