abecb084d2eb94ab9e58f610feee6dd059fe695b
Esteban <cepalacios@outlook.com>
2020-06-11 09:02:03 -0500

    tests for secondary constructors

--- src/inventory/items.kt
+++ src/inventory/items.kt
@@ -10,31 +10,31 @@ abstract class BaseItem {
     abstract val statusModifier: String?
 }
 
-class Meat : BaseItem(), UseItem, ChooseItem {
-    override val itemName = "meat"
-    override val healthModifier = 3
-    override val statusModifier: String? = null
-
-    override fun useItem(item: BaseItem, petZombie: Zombie) {
-        if (petZombie.hitPoints < petZombie.baseHitPoints) {
-            petZombie.hitPoints += item.healthModifier!!
-            println(
-                "${petZombie.name} ate the meat and now has ${petZombie.hitPoints}/${petZombie.baseHitPoints} hit " +
-                        "points"
-            )
-        } else {
-            println("${petZombie.name} is at full health")
-            chooseItem()
-        }
-    }
-}
-
-class Rabies : BaseItem() {
-    override val itemName = "rabies"
-    override val statusModifier = "angry"
-    override val healthModifier: Int? = null
-}
-
-interface UseItem {
-    fun useItem(item: BaseItem, petZombie: Zombie) {}
-}
+//class Meat : BaseItem(), UseItem, ChooseItem {
+//    override val itemName = "meat"
+//    override val healthModifier = 3
+//    override val statusModifier: String? = null
+//
+//    override fun useItem(item: BaseItem, petZombie: Zombie) {
+//        if (petZombie.hitPoints < petZombie.baseHitPoints) {
+//            petZombie.hitPoints += item.healthModifier!!
+//            println(
+//                "${petZombie.name} ate the meat and now has ${petZombie.hitPoints}/${petZombie.baseHitPoints} hit " +
+//                        "points"
+//            )
+//        } else {
+//            println("${petZombie.name} is at full health")
+//            chooseItem()
+//        }
+//    }
+//}
+//
+//class Rabies : BaseItem() {
+//    override val itemName = "rabies"
+//    override val statusModifier = "angry"
+//    override val healthModifier: Int? = null
+//}
+//
+//interface UseItem {
+//    fun useItem(item: BaseItem, petZombie: Zombie) {}
+//}
