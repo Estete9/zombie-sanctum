f1f667455b9f0addc0f57f4466a0f79f2309ca10
Estete9 <cepalacios@outlook.com>
2020-06-27 12:55:55 -0500

    commented the println for a test

--- src/Main.kt
+++ src/Main.kt
@@ -11,22 +11,22 @@ fun main() {
     var pInput = readLine()!!
     val player = Player(pInput)
 
-    println("No time for explanations! here comes the first one!")
+//    println("No time for explanations! here comes the first one!")
 
     val zombie1 = FriendlyZombie()
     println("\n${zombie1.name} has appeared! \n${zombie1.hitPoints} hit points, tamable ${zombie1.changingTamable}")
 
-    println("what would you do? \npunch, run or pet?")
+//    println("what would you do? \npunch, run or pet?")
     player.makeBattleDecision(player, null, zombie1, player.unitStrength)
 
-    println("Let's see your new friend")
+//    println("Let's see your new friend")
     openInventory()
-    println("Phew, that was close. \nhello ${player.name}")
-    println("oh here comes another one, get ready!")
+//    println("Phew, that was close. \nhello ${player.name}")
+//    println("oh here comes another one, get ready!")
 
     val zombie2 = BasicZombie()
-    println("\n${zombie2.name} has appeared! \n${zombie2.hitPoints} hit points")
-    println("let's ask our new friend for help. \nChoose \"zombie bite\"")
+//    println("\n${zombie2.name} has appeared! \n${zombie2.hitPoints} hit points")
+//    println("let's ask our new friend for help. \nChoose \"zombie bite\"")
 
     selectedPetZombie = retrieveZombie(pInput)
     player.makeBattleDecision(player, selectedPetZombie.second, zombie2, player.unitStrength)
